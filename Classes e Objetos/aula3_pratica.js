15610612e920628d33eae5a552a924ec580a5bda
Adriano <adpsants@gmail.com>
2022-11-06 22:07:32 -0300

    minha primeira tentativa de resolução do problema, algo não deu certo

--- Classes e Objetos/aula3_pratica.js
+++ Classes e Objetos/aula3_pratica.js
@@ -1,31 +1,65 @@
+/*
 class Pessoa {
-    id;
     nome;
     idade;
     anoDeNascimento;
 
-    constructor(id, nome, idade){
-        this.id = id;
+    constructor(nome, idade){
         this.nome = nome;
         this.idade = idade;
         this.anoDeNascimento = 2022 - idade;
     }
 
     descrever(){
-        console.log(`Meu id é ${this.id} meu nome é ${this.nome} e a minha idade é ${this.idade}`);
+        console.log(`Meu nome é ${this.nome} e a minha idade é ${this.idade}`);
     }
 }
 
-const adriano = new Pessoa('1112785', 'Adriano', 28);
+function compararPessoa(p1, p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velha que ${p2.nome}`);
+    } else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velha que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
+    }
+}
 
-console.log(adriano);
+const adriano = new Pessoa('Adriano', 28);
+const helena = new Pessoa('Helena', 7);
+
+compararPessoa(adriano, helena);
+*/
+
+/*
+    1) Crie uma classe para representar carros. 
+    Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
+    Crie um método que dado a quantidade de quilómetros e o preço do combustível nos dê o valor 
+    gasto em reais para realizar este percurso.
+*/
 
 
+class Carro{
+    marca;
+    cor;
+    litrosPorKm;
+
+    constructor(marca, cor, litrosPorKm){
+        this.marca = marca;
+        this.cor = cor;
+        this.litrosPorKm = litrosPorKm;
+    }
 
 
+}
 
+function valorGasto(totalKmPercorrido, precoCombustível){
+    return hb20.litrosPorKm;
+}
 
+const hb20 = new Carro('hiunday', 'vermelho', 10);
 
+valorGasto(hb20);
 
 
 
