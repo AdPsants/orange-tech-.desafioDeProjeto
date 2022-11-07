e61fe5f0a8e16852eabcb92a020008d2f066c623
Adriano <adpsants@gmail.com>
2022-11-06 23:05:07 -0300

    primeira resolução do desafio de IMC com class, a pessoa ainda não fala mas já tenho o IMC

--- Classes e Objetos/aula3_pratica.js
+++ Classes e Objetos/aula3_pratica.js
@@ -62,7 +62,33 @@ const ferrari = new Carro('Ferrari', 'Azul', 1/15);
 
 console.log(hb20.valorGasto(100, 5.79));
 
+/*
+2) Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
+do seu IMC;
+*/
 
+class Pessoa{
+    nome;
+    peso;
+    altura;
+
+    constructor(nome, peso, altura){
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    imc(peso, altura){
+        return (this.peso / Math.pow(this.altura, 2)).toFixed(2);
+    }
+}
+
+const jose = new Pessoa('José', 70, 1.70);
+
+console.log(jose.imc());
 
 
 
