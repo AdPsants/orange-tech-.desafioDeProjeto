443a562ce1491638ed6623161c4a77620b92e78f
Adriano <adpsants@gmail.com>
2022-11-15 15:53:22 -0300

    Novas requisições para API e manipulação dos dados recebidos

--- Pockedex/assets/js/main.js
+++ Pockedex/assets/js/main.js
@@ -1,19 +1,20 @@
 
-
+function convertPokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function converterPokemonToLi(pokemon){
     return `
         <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poision</li>
+              ${convertPokemonTypesToLi(pokemonTypes).join(' ')}  
             </ol>
             
-            <img src="https://seeklogo.com/images/B/bulbasaur-logo-8A27924C02-seeklogo.com.png" alt="${pokemon.name}">
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
         </div>
         </li>
     `
