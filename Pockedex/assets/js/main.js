41d4c08da78aa71d09b30d188a2591c7fd3d2bb8
Adriano <adpsants@gmail.com>
2022-11-15 20:49:02 -0300

    consumindo os detalhes dos pokemons

--- Pockedex/assets/js/main.js
+++ Pockedex/assets/js/main.js
@@ -1,24 +1,26 @@
 
-function convertPokemonTypesToLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+function convertPokemonTypesToLi (pokemonTypes){
+    return pokemonTypes.map( (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
 }
 
 function converterPokemonToLi(pokemon){
     return `
         <li class="pokemon">
-        <span class="number">${pokemon.order}</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-              ${convertPokemonTypesToLi(pokemonTypes).join(' ')}  
+                ${convertPokemonTypesToLi(pokemon.types).join(' ')}
             </ol>
             
-            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+           <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
         </div>
         </li>
     `
 }
+
+
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons() .then((pokemons = []) => { 
